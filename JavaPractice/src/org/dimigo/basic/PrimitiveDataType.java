6399854f6e130b4505795e3491c795cf796eb4fa
zip_s <zip_s@DESKTOP-SE4J4UU>
2017-03-10 15:22:14 +0900

    실습과제1 제출

---
+++ JavaPractice/src/org/dimigo/basic/PrimitiveDataType.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ *   |_ PrimitiveDataType.java
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 10.
+ * </pre>
+ *
+ * @author : zip_s
+ * @version : 1.0
+ */
+public class PrimitiveDataType {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
