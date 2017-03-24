1534dadfba12cfb0e233d737d0cd43b0d157e67b
zip_s <zip_s@DESKTOP-SE4J4UU>
2017-03-24 15:26:37 +0900

    Car2

---
+++ JavaPractice/src/org/dimigo/oop/CarTest2.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_ CarTest.java
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ *
+ * @author : zip_s
+ * @version : 1.0
+ */
+public class CarTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Car2 car2 = new Car2("현대자동차","제네시스","검정색",225,50000000);
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + car2.getCompany());
+		System.out.println("모델명 : " + car2.getModel());
+		System.out.println("색상 : " + car2.getColor());
+		System.out.println("최대속도 : " + car2.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d",car2.getPrice()) + "원");
+		System.out.println("");
+		Car2 car21 = new Car2("기아자동차","K7","흰색",246,40000000);
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + car21.getCompany());
+		System.out.println("모델명 : " + car21.getModel());
+		System.out.println("색상 : " + car21.getColor());
+		System.out.println("최대속도 : " + car21.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d",car21.getPrice()) + "원");
+		System.out.println("");
+		Car2 car211 = new Car2("삼성자동차","SM7","회색",200,38000000);
+		System.out.println("<< 자동차 목록 >>");
+		System.out.println("제조사명 : " + car211.getCompany());
+		System.out.println("모델명 : " + car211.getModel());
+		System.out.println("색상 : " + car211.getColor());
+		System.out.println("최대속도 : " + car211.getMaxSpeed() + "km");
+		System.out.println("가격 : " + String.format("%,d",car211.getPrice()) + "원");
+	}
+
+}
