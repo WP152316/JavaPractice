3392e07a91f140cf036767b405e3df611f14a766
ZIPsc <didtmdcjf2544@gmail.com>
2017-05-12 15:38:40 +0900

    SmartPhone

---
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -0,0 +1,39 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_ SmartPhoneTest.java
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 12.
+ * </pre>
+ *
+ * @author : zip_s
+ * @version : 1.0
+ */
+public class SmartPhoneTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		SmartPhone[] s = {
+				new IPhone("IPhone 7","애플",900000),
+				new Galaxy("갤럭시 S8","삼성",800000)
+		};
+		for (SmartPhone sm : s){
+			System.out.println(sm);
+			sm.turnOn();
+			sm.pay();
+			sm.useSpecialFunction(sm);
+			sm.turnoff();
+			System.out.println("");
+		}
+		
+	}
+
+}
