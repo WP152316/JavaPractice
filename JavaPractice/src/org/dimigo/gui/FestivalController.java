df69e41bcd954294a7a285266456dcc49792a44f
ZIPsc <didtmdcjf2544@gmail.com>
2017-06-22 11:50:58 +0900

    dddddds

---
+++ JavaPractice/src/org/dimigo/gui/FestivalController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.dimigo.gui;
+
+/**
+ * <pre>
+ * org.dimigo.gui
+ *   |_ FestivalController.java
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 6. 22.
+ * </pre>
+ *
+ * @author : zip_s
+ * @version : 1.0
+ */
+public class FestivalController {
+
+}
---
+++ JavaPractice/src/org/dimigo/gui/FestivalMain.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.dimigo.gui;
+
+/**
+ * <pre>
+ * org.dimigo.gui
+ *   |_ FestivalMain.java
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 6. 22.
+ * </pre>
+ *
+ * @author : zip_s
+ * @version : 1.0
+ */
+public class FestivalMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
